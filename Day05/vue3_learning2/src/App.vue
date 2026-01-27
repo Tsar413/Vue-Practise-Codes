f55c865b8782c0b220fc1240379e4282ef13e138
Tsar413 <xiaolongbao413@163.com>
2026-01-27 10:59:29 +0800

    Router Learning 1

--- Day05/vue3_learning2/src/App.vue
+++ Day05/vue3_learning2/src/App.vue
@@ -1,26 +1,68 @@
 <template>
   <div class="app">
-    <!-- <watchEffectLearning />
-    <TagRef /> -->
-    <!-- <TypeScriptsReview /> -->
-    <PropsLearning :list="personList" />  
-    <HooksLearning />
+    <h2 class="title">Vue路由测试</h2>
+    <!-- 导航区 -->
+    <div class="navigate">
+      <RouterLink to="/home">首页</RouterLink>
+      <RouterLink to="/news">新闻</RouterLink>
+      <RouterLink to="/about">关于</RouterLink>
+    </div>
+    <!-- 展示区 -->
+    <div class="main-content">
+      <RouterView></RouterView>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup name="App">
-  import WatchEffectLearning from './components/WatchEffectLearning.vue';
-  import TagRef from './components/TagRef.vue';
-  import TypeScriptsReview from './components/TypeScriptsReview.vue';
-  import PropsLearning from './components/PropsLearning.vue';
-  import HooksLearning from './components/HooksLearning.vue';
-  import { reactive } from 'vue';
-  import { type Persons } from '@/types'
-
-  let personList = reactive<Persons>([
-    { id: '1', name: '张三', age: 30 },
-    { id: '2', name: '李四', age: 18 },
-    { id: '3', name: '王五', age: 5, x: 9 }
-  ])
+  import {RouterView, RouterLink} from 'vue-router'
 
 </script>
+
+<style>
+    /* App */
+  .title {
+    text-align: center;
+    word-spacing: 5px;
+    margin: 30px 0;
+    height: 70px;
+    line-height: 70px;
+    background-image: linear-gradient(45deg, gray, white);
+    border-radius: 10px;
+    box-shadow: 0 0 2px;
+    font-size: 30px;
+  }
+  .navigate {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 100px;
+  }
+  .navigate a {
+    display: block;
+    text-align: center;
+    width: 90px;
+    height: 40px;
+    line-height: 40px;
+    border-radius: 10px;
+    background-color: gray;
+    text-decoration: none;
+    color: white;
+    font-size: 18px;
+    letter-spacing: 5px;
+  }
+  .navigate a.xiaozhupeiqi {
+    background-color: #64967E;
+    color: #ffc268;
+    font-weight: 900;
+    text-shadow: 0 0 1px black;
+    font-family: 微软雅黑;
+  }
+  .main-content {
+    margin: 0 auto;
+    margin-top: 30px;
+    border-radius: 10px;
+    width: 90%;
+    height: 400px;
+    border: 1px solid;
+  }
+</style>
