6a7fde62b7dc4bd9e51bbed80e666452b46cb626
Tsar413 <xiaolongbao413@163.com>
2026-02-05 12:38:23 +0800

    Pinia Learning1

--- Day06/vue_learning3/src/App.vue
+++ Day06/vue_learning3/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Count/>
+  <br>
+  <LoveTalk/>
 </template>
 
-<style scoped></style>
+<script setup lang="ts" name="App">
+  import Count from './components/Count.vue'
+  import LoveTalk from './components/LoveTalk.vue'
+</script>
--- Day06/vue_learning3/src/main.ts
+++ Day06/vue_learning3/src/main.ts
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+// 引入createApp用于创建应用
+import {createApp} from 'vue'
+// 引入App根组件
 import App from './App.vue'
+// 第一步 引入Pinia
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+// 创建一个应用
+const app = createApp(App)
+// 第二步 创建Pinia
+const pinia = createPinia()
+// 第三步 安装Pinia
+app.use(pinia)
+// 挂载整个应用到app容器中
+app.mount('#app')
