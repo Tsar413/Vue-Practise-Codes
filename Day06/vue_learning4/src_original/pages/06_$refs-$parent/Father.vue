43f2fe281ccf2646632a93f75116c3a82bc2a56f
Tsar413 <xiaolongbao413@163.com>
2026-02-14 21:12:02 +0800

    Component Communication Preparation

---
+++ Day06/vue_learning4/src_original/pages/06_$refs-$parent/Father.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="father">
+		<h3>父组件</h3>
+
+		<Child1 />
+		<Child2 />
+	</div>
+</template>
+
+<script setup lang="ts" name="Father">
+	import Child1 from './Child1.vue'
+	import Child2 from './Child2.vue'
+
+
+	// 注意点：当访问obj.c的时候，底层会自动读取value属性，因为c是在obj这个响应式对象中的
+	/* let obj = reactive({
+		a:1,
+		b:2,
+		c:ref(3)
+	})
+	let x = ref(4)
+
+	console.log(obj.a)
+	console.log(obj.b)
+	console.log(obj.c)
+	console.log(x) */
+	
+
+
+</script>
+
+<style scoped>
+	.father {
+		background-color: rgb(165, 164, 164);
+		padding: 20px;
+		border-radius: 10px;
+	}
+
+	.father button {
+		margin-bottom: 10px;
+		margin-left: 10px;
+	}
+</style>
+
